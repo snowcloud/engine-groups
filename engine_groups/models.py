d3b8d5896a0ff2abdcc18010577e843f8eac4e7a
Derek Hoy <derek@snowcloud.co.uk>
2011-04-05 21:50:48 +0100

    added order_by name to Account

--- engine_groups/models.py
+++ engine_groups/models.py
@@ -9,23 +9,9 @@ from mongoengine.django.auth import User
 MEMBER_ROLE = 'member'
 ADMIN_ROLE = 'admin'
 MENTOR_ROLE = 'mentor'
-
-# mongoengine.django.auth.User
-
-# class User(Document):
-#     """A User document that aims to mirror most of the API specified by Django
-#     at http://docs.djangoproject.com/en/dev/topics/auth/#users
-#     """
-#     username = StringField(max_length=30, required=True)
-#     first_name = StringField(max_length=30)
-#     last_name = StringField(max_length=30)
-#     email = StringField()
-#     password = StringField(max_length=128)
-#     is_staff = BooleanField(default=False)
-#     is_active = BooleanField(default=True)
-#     is_superuser = BooleanField(default=False)
-#     last_login = DateTimeField(default=datetime.datetime.now)
-#     date_joined = DateTimeField(default=datetime.datetime.now)
+STATUS_OK = 'ok'
+STATUS_SUSPENDED = 'suspended'
+STATUS_CLOSED = 'closed'
 
     
 class Membership(EmbeddedDocument):
@@ -49,12 +35,12 @@ class Account(Document):
     api_key = StringField(max_length=64)
     api_password = StringField(max_length=64)
     members = ListField(EmbeddedDocumentField(Membership))
+    status = StringField(max_length=12, default=STATUS_OK )
     
-    
-    
-    # resources = ListField(ReferenceField(Resource))
-    # users = ListField(ReferenceField(User))
-    # emails = ListField(EmailField())
+    meta = {
+        'ordering': ['name']
+    }
+            
 
     # created_by = ReferenceField(User)
     # created_at = DateTimeField(default=datetime.datetime.now)
