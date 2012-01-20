4d7885d1221513808682e5b7fa1085aa5e8ca859
Derek Hoy <derek@snowcloud.co.uk>
2012-01-20 18:10:53 +0000

    updated tests and fixed dumping/loading collections.

--- engine_groups/tests.py
+++ engine_groups/tests.py
@@ -3,99 +3,98 @@
 engine-groups/tests.py
 """
 
-from django.test import TestCase, TransactionTestCase
-from engine_groups.models import Account, Membership, MEMBER_ROLE, ADMIN_ROLE
+import os
 
-# # from firebox.ordnancesurvey import get_os_postcode, \
-# #     OS_LABEL, OS_TYPE, OS_LAT, OS_LON, OS_WARD, OS_DISTRICT, OS_COUNTRY
-# from firebox.views import *
-# 
-# from depot.models import Resource, Location, load_resource_data, \
-#     get_place_for_postcode, lat_lon_to_str, get_location_for_postcode
-# from depot.forms import ShortResourceForm
-# from mongoengine import connect
-# from mongoengine.connection import _get_db as get_db
-# 
-# TEST_DB_NAME = 'test_db2'
-# 
-# # TEST_URL = "http://www.gilmerton.btik.com/p_Pilates.ikml"
-# TEST_URL = "http://www.gilmerton.btik.com/"
-# TEST_URL_RESULT = 3
-# TEST_URL2 = "http://www.anguscardiacgroup.co.uk/Programme.htm"
-# TEST_URL2_RESULT = 20
-# 
+from django.contrib.auth.models import User
+from mongoengine.connection import _get_db as get_db
+from mongoengine.django.tests import MongoTestCase
+
+from engine_groups.models import Account, Membership, MEMBER_ROLE, ADMIN_ROLE
+from engine_groups.collections import Collection
+
 SEP = '**************'
 
-import json
-from mongoengine import base as mongobase
-from pymongo import json_util
+# import json
+# from mongoengine import base as mongobase
+# from pymongo import json_util
 
-def _dump_accounts(fname):
-    datas = []
-    for u in Account.objects.all():
-       datas.append(u.to_mongo())
-    data = json.dumps(datas, sort_keys=True, indent=4, default=json_util.default) # Write to file.
-    o = open(fname, 'w')
-    o.write(data)
-    o.close()
+def _dump_collections(collection_names=None):
+    if collection_names is None:
+        collection_names = [coll for coll in get_db().collection_names() if coll != 'system.indexes']
+    for coll in collection_names:
+        os.system('mongoexport -d {0} -c {1} -o output_{1}.json'.format(get_db().name, coll))
 
-def _load_accounts(fname):
-    """docstring for _load_accounts"""
-    f = open(fname, 'r') # JSON file
-    datas = json.loads(f.read(), object_hook=json_util.object_hook )
-    for data in datas:
-        model = mongobase._document_registry[data['_cls']]
-        model_instance = model._from_son(data)
-        model_instance.save(validate = False) # Ingore Ref fields
+def _load_collections(collection_names=None, drop='--drop'):
+    if collection_names is None:
+        collection_names = [coll for coll in get_db().collection_names() if coll != 'system.indexes']
+    for coll in collection_names:
+        os.system('mongoimport -d {0} -c {1} --file output_{1}.json {2}'.format(get_db().name, coll, drop))
 
 def _print_db_info():
     """docstring for _print_db_info"""
     print SEP
     print 'Account: ', Account.objects.count()
-    # print 'Membership: ', Membership.objects.count()
+    print 'Membership: ', Membership.objects.count()
     print SEP
-    
-class AccountTest(TransactionTestCase):
+
+def _make_user(name):
+    return User.objects.create_user(name, email="%s@example.com" % name, password='password')
+
+class AccountsBaseTest(MongoTestCase):
     def setUp(self):
-        _print_db_info()
+        # _print_db_info()
+        self.user_humph = _make_user('bob')
+        self.user_jorph = _make_user('jorph')
+        self.user_group = _make_user('group')
+
+        self.humph = Account.objects.create(name="Humph Floogerwhippel", email="humph@example.com", local_id=str(self.user_humph.id))
+        self.jorph = Account.objects.create(name="Jorph Wheedjilli", email="jorph@example.com", local_id=str(self.user_jorph.id))
+        self.group = Account.objects.create(
+            name="Flupping Baxters of Falkirk", 
+            email="group@example.com", 
+            local_id=str(self.user_group.id),
+            )
+        self.group.add_member(self.humph)
+        self.group.add_member(self.jorph, role=ADMIN_ROLE)
 
     def tearDown(self):
-        _print_db_info()
+        # _print_db_info()
+        pass
         
         # _dump_accounts('blah.json')
-        print 'dropping Account docs'
-        Account.drop_collection()
+        # print 'dropping Account docs'
+        # Account.drop_collection()
         # Membership.drop_collection()
-    
+
+class CollectionsTest(AccountsBaseTest):
+
+    def test_creation(self):
+        c = Collection()
+        # print 'wulp', c
+
+class AccountsTest(AccountsBaseTest):
     def test_account(self):
-        acct1 = Account(name='Humph Floogerwhippel')
-        acct1.save()
-        acct2 = Account(name='Jorph Wheedjilli')
-        acct2.save()
-        # print acct.name
-        group = Account(name='Flupping Baxters of Falkirk', 
-            members=[Membership(member=acct1), Membership(member=acct2, role=ADMIN_ROLE)])
-        group.save()
-        print group
-        print [m for m in group.members]
+
+        # print self.group
+        # print [m for m in self.group.members]
         self.assertEqual(Account.objects.count(), 3)
-        _dump_accounts('blah.json')
+        _dump_collections()
 
     def test_load_accounts(self):
         
         """docstring for test_load_accounts"""
-        _load_accounts('blah.json')
+        _load_collections()
         self.assertEqual(Account.objects.count(), 3)
-        group = Account.objects.get(name='Flupping Baxters of Falkirk')
-        acct1 = Account(name='Arthur Sixpence')
-        acct1.save()
+        # group = Account.objects.get(name='Flupping Baxters of Falkirk')
+        arthur = _make_user('arthur')
+        acct1 = Account.objects.create(name="Arthur Sixpence", email="arthur@example.com", local_id=str(arthur.id))
         # print acct.name
-        group.members.append(Membership(member=acct1))
-        group.save()
-        self.assertEqual(len(group.members), 3)
+        self.group.add_member(acct1)
+        self.group.save()
+        self.assertEqual(len(self.group.members), 3)
 
-        print group
-        print [m for m in group.members]
+        print self.group
+        print [m for m in self.group.members]
         
         
         
